601f8d16fcb65b1331a9c6991a20d91021beea1a
Yext Studio <studio-placeholder@yext.com>
2023-07-27 05:12:46 +0000

    Yext Studio Commit

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -1,5 +1,6 @@
 import { GetPath, TemplateProps } from "@yext/pages";
 import Footer from "../components/Footer";
+import HeaderSimple from "../components/HeaderSimple";
 import Headline from "../components/Headline";
 
 export const getPath: GetPath<TemplateProps> = () => {
@@ -9,6 +10,7 @@ export const getPath: GetPath<TemplateProps> = () => {
 export default function Home() {
   return (
     <>
+      <HeaderSimple backgroundColor="#FFFFFF" logo={``} />
       <Headline value="Headline" textSize="xl" fontWeight="medium" />
       <Footer />
     </>
